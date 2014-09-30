af809034e61e2ed09b64bf06305cd319128d2fb0
Adam Carter <adamrcarter@gmail.com>
2014-09-30 16:43:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Visdir::Application.config.secret_token = '33d330d6ea2410aad6f413e16351688e4f94258263c27dbcae1fba76078e8c096b32cfc54cb7707140c59b666ae8456dd469dc93cae1ed039b5a7d4fb9e5e66c'
