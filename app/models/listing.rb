f639f7faa429df155030c08af4a3ce3d859f46f1
Adam Carter <adamrcarter@gmail.com>
2014-09-30 17:13:04 -0500

    added relationships between the models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,5 @@
 class Listing < ActiveRecord::Base
   attr_accessible :budget, :category, :high_budget, :location, :low_budget, :photo
+  
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation, :pro, :regular
   
+  has_many :listings
+  
   validates :email, uniqueness: true
   validates :name, :email, :password, :password_confirmation, presence: true
 end
