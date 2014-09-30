8707f251832ec745cd7bdbc1199f24814da2ed60
Adam Carter <adamrcarter@gmail.com>
2014-09-30 17:55:07 -0500

    added image uploader to the Listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,7 @@ class Listing < ActiveRecord::Base
   attr_accessible :budget, :category, :high_budget, :location, :low_budget, :photo
   
   belongs_to :user
+  
+  mount_uploader :photo, ImageUploader #did rails g uploader photo
 end
+#forgot to add a :title, need to do a migration for that...
