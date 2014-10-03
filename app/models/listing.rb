f27d8e62b02bfaff8c0d87262802fb6bcb962e2b
Adam Carter <adamrcarter@gmail.com>
2014-10-02 21:27:36 -0500

    wip

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,6 +4,8 @@ class Listing < ActiveRecord::Base
   belongs_to :user
   
   mount_uploader :photo, ImageUploader #did rails g uploader photo
+  
+  accepts_nested_attributes_for :avatar, :allow_destroy => true, :reject_if => :avatar_is_blank
 end
 
 #remove low_ and high_ budget fields? just use budget? or make them boolean?
